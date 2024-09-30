53a49c92461191ca06501d2b907d76cc086da5c1
Elazar Porush <lzrprws@gmail.com>
2024-09-30 11:37:07 +0300

    create defoulte data

---
+++ src/data/data.ts
@@ -0,0 +1,17 @@
+import Todo from "../types/models/todo"
+import User from "../types/models/user"
+
+export const users: User[] = []
+export const todos: Todo[] = [];
+
+(async (): Promise<void> => {
+    const johnny: User = new User("Johnny")
+    await johnny.hashPassword("1234")
+    
+    const laundry: Todo = new Todo("Fold fome clean laundry", johnny.id)
+    const dishes: Todo = new Todo("Make the dishes from Shabbat dinner", johnny.id)
+
+    users.push(johnny)
+    todos.push(laundry, dishes)
+})()
+
