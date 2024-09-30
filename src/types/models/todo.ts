09f7c28a716f460d3be9d140bd8d3d506b5138f9
Elazar Porush <lzrprws@gmail.com>
2024-09-30 10:25:33 +0300

    todo model done

--- src/types/models/todo.ts
+++ src/types/models/todo.ts
@@ -1,7 +1,16 @@
-// export default class Todo {
-//     constructor(
-//         public 
-//     ){
+import { v4 } from "uuid"
 
-//     }
-// }
+export default class Todo {
+    public id: string
+    public completed: boolean = false
+    constructor(
+        public title: string,
+        public userId: string
+    ){
+        this.id = v4()
+    }
+
+    toggleStatus(): void {
+        this.completed = !this.completed
+    }
+}
